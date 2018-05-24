4cb075a1d09aa61eff382b3655df28d1df1a11d7
Kelsey Oates <kelsey.oates@gmail.com>
2018-05-24 19:15:44 -0400

    Update Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -20,7 +20,7 @@ const Work = () => (
         <h3>Joanne B. Lannon, M.ED., DLM (ASCP)</h3>
         <h5>Founder & President</h5>
           <p>Joanne Lannon, President and Founder of South Shore Laboratory Consultants, has over 25 years of experience in laboratory medicine, and has provided consulting services to many types of clinical laboratories since 1988.</p>
-          <p>As one of the first laboratory consultants in the state of Massachusetts, she has used her vast experience to maintain an exceptional reputation within the industry. Joanne is always highly recommended by professionals in her field.</p>
+          <p>the state of Massachusetts, she has used her vast experience to maintain an exceptional reputation within the industry. Joanne is always highly recommended by professionals in her field.</p>
           <p>She has a Masters Degree in Education and is certified by the American Society of Clinical Pathologists as a Diplomate in Laboratory Management.</p>
       </div>
 
@@ -58,7 +58,7 @@ const Work = () => (
       <img className="team" src={glennPic} alt="" />
         <h3>Glenn Bouchie, MT (ASCP)</h3>
         <h5>Senior Regulatory Consultant</h5>
-          <p>Glenn Bouchie is an ASCP Board registered Medical Technologist. He has functioned as a Laboratory Manager, Senior Research Scientist, Principle Investigator, CAP Inspector, and Technical Supervisor with over 25 years of experience in method research and development, regulatory compliance, laboratory operations management, quality assurance, and process improvement in Research, Hospital, Clinical Diagnostic, Drug Development, and Life Sciences settings.</p>
+          <p>Glenn Bouchie is an ASCP Board registered Medical Technologist.</p> <p>He has functioned as a Laboratory Manager, Senior Research Scientist, Principle Investigator, CAP Inspector, and Technical Supervisor with over 25 years of experience in method research and development, regulatory compliance, laboratory operations management, quality assurance, and process improvement in Research, Hospital, Clinical Diagnostic, Drug Development, and Life Sciences settings.</p>
       </div>
 
       <div className="member-social">
@@ -73,9 +73,10 @@ const Work = () => (
     <div className="six columns header-col">
       <div className="team-member">
       <img className="team" src={lizPic} alt="" />
-        <h3>Elizabeth Mullady, MLS(ASCP)CM, MHA</h3>
+        <h3>Elizabeth Mullady, MT (ASCP)</h3>
         <h5>Senior Regulatory Consultant</h5>
-          <p>Elizabeth Mullady is an ASCP Board registered Medical Laboratory Scientist with a Masters degree in Health Care Administration. She has 15 years of experience in clinical laboratories, first as a technologist in the Infection Disease Diagnostic lab at Boston Children’s Hospital and then in the quality assurance and regulatory affairs field in a molecular genetics lab. In her role as Director of Quality Assurance and Regulatory Affairs she was responsible to maintain the lab in a constant state of regulatory readiness while keeping the wheel of continual process improvement rolling. Elizabeth led the lab through the renewal of their CLIA certificate, gaining ISO 15189 accreditation, and securing licenses with Massachusetts, Rhode Island, Pennsylvania, Maryland, Florida, California, and New York State.</p>
+          <p>Elizabeth Mullady is an ASCP Board registered Medical Laboratory Scientist with a Masters degree in Health Care Administration. She has 15 years of experience in clinical laboratories, first as a technologist in the Infection Disease Diagnostic lab at Boston Children’s Hospital and then in the quality assurance and regulatory affairs field in a molecular genetics lab. </p>
+          <p>In her role as Director of Quality Assurance and Regulatory Affairs she was responsible to maintain the lab in a constant state of regulatory readiness while keeping the wheel of continual process improvement rolling. Elizabeth led the lab through the renewal of their CLIA certificate, gaining ISO 15189 accreditation, and securing licenses with Massachusetts, Rhode Island, Pennsylvania, Maryland, Florida, California, and New York State.</p>
       </div>
 
       <div className="member-social">
